268fc67c549892cd90c92482c92b2e5e0475dfcc
Santiago Torifa Manso <119546420+Krosbift@users.noreply.github.com>
2024-04-26 10:00:53 -0500

    formato nuevo para el archivo FIZZ_BUZZ.c

--- C/FIZZ_BUZZ.c
+++ C/FIZZ_BUZZ.c
@@ -12,15 +12,15 @@
 void fizzbuzz();
 
 int main (){
-	fizzbuzz();
-	return 0;
+    fizzbuzz();
+    return 0;
 }
 
 void fizzbuzz() {
-	for (int i = 1; i <= 100; i++) {
-		if (i % 3 == 0 && i % 5 != 0) printf("fizz\n");
-		if (i % 5 == 0 && i % 3 != 0) printf("buzz\n");
-	  if (i % 3 == 0 && i % 5 == 0) printf("fizzbuzz\n");
-		if ((i % 3 != 0) && (i % 5 != 0)) printf("%d\n", i);
-	}
+    for (int i = 1; i <= 100; i++) {
+	if (i % 3 == 0 && i % 5 != 0) printf("fizz\n");
+	if (i % 5 == 0 && i % 3 != 0) printf("buzz\n");
+	if (i % 3 == 0 && i % 5 == 0) printf("fizzbuzz\n");
+	if ((i % 3 != 0) && (i % 5 != 0)) printf("%d\n", i);
+    }
 }
