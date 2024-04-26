d8d3e417f54178488ef276bc42acb02cbb9b3436
SETellez <tellezmariana08@gmail.com>
2024-04-25 20:15:16 -0500

    ejercicio fizzbuzz. CHECK

---
+++ C/FIZZ_BUZZ.c
@@ -0,0 +1,31 @@
+/*
+Escribe un programa que muestre por consola (con un print) los
+números de 1 a 100 (ambos incluidos y con un salto de línea entre
+cada impresión), sustituyendo los siguientes:
+Múltiplos de 3 por la palabra "fizz".
+Múltiplos de 5 por la palabra "buzz".
+Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+
+hacer lo de la main en otra funcion
+ponerle comentarios de cada cosa
+hacer el mismo codigo sin usar else 
+
+*/
+
+#include <stdio.h>
+
+void fizzbuzz();
+
+int main (){
+	fizzbuzz();
+	return 0;
+}
+
+void fizzbuzz() {
+	for (int i = 1; i <= 100; i++) {
+		if (i % 3 == 0 && i % 5 != 0) printf("fizz\n");
+		if (i % 5 == 0 && i % 3 != 0) printf("buzz\n");
+	  if (i % 3 == 0 && i % 5 == 0) printf("fizzbuzz\n");
+		if ((i % 3 != 0) && (i % 5 != 0)) printf("%d\n", i);
+	}
+}
